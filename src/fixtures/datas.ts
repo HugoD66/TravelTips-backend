a4f6ede6270232c8b18cb52f0d874490af63eca3
HugoD66 <dessauw.hugo@gmail.com>
2024-04-29 11:34:23 +0200

    Temp correction dayItinerary

--- src/fixtures/datas.ts
+++ src/fixtures/datas.ts
@@ -412,7 +412,7 @@ export class Mockups {
       nbApprovate: 2,
       public: true,
       approvate: TipsApprovate.Approvate,
-      dayItinerary: ['1'],
+      dayItinerary: [],
       idCategory: category[Math.floor(Math.random() * category.length)].id,
       idUser: user[Math.floor(Math.random() * user.length)].id,
     });
